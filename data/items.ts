3c2f871ddef3ee9f451bbda789875dca9b195234
Khaled <khalegjr@gmail.com>
2023-02-05 03:05:29 -0300

    Cria dados para simulação

---
+++ data/items.ts
@@ -0,0 +1,34 @@
+import { Item } from "./../src/types/Item";
+
+export const items: Item[] = [
+  {
+    date: new Date(2022, 12, 21),
+    category: "food",
+    title: "Pizza",
+    value: 55.9,
+  },
+  {
+    date: new Date(2023, 1, 2),
+    category: "food",
+    title: "Cerveja",
+    value: 68.0,
+  },
+  {
+    date: new Date(2023, 1, 10),
+    category: "rent",
+    title: "Alguel",
+    value: 2549,
+  },
+  {
+    date: new Date(2023, 1, 21),
+    category: "food",
+    title: "lanche",
+    value: 45.9,
+  },
+  {
+    date: new Date(2023, 1, 7),
+    category: "sarlary",
+    title: "Salário ACME",
+    value: 5500,
+  },
+];
