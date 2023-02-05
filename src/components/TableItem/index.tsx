f2296ca36707de4310f556eca2eb85ef5c8141a2
Khaled <khalegjr@gmail.com>
2023-02-05 16:29:00 -0300

    Implementa componentes de destaque de informações

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,7 @@
+import { categories } from "../../data/categories";
 import { formatDate } from "../../helpers/dateFilters";
 import { Item } from "../../types/Item";
-import { TableColumn, TableLine } from "./styles";
+import { Category, TableColumn, TableLine, Value } from "./styles";
 
 type Props = {
   item: Item;
@@ -10,9 +11,17 @@ export const TableItem = ({ item }: Props) => {
   return (
     <TableLine>
       <TableColumn>{formatDate(item.date)}</TableColumn>
-      <TableColumn>{item.category}</TableColumn>
+      <TableColumn>
+        <Category color={categories[item.category].color}>
+          {categories[item.category].title}
+        </Category>
+      </TableColumn>
       <TableColumn>{item.title}</TableColumn>
-      <TableColumn>R$ {item.value}</TableColumn>
+      <TableColumn>
+        <Value color={categories[item.category].expense ? "red" : "green"}>
+          R$ {item.value}
+        </Value>
+      </TableColumn>
     </TableLine>
   );
 };
--- src/components/TableItem/styles.ts
+++ src/components/TableItem/styles.ts
@@ -5,3 +5,14 @@ export const TableLine = styled.tr``;
 export const TableColumn = styled.td`
   padding: 10px 0;
 `;
+
+export const Category = styled.span<{ color: string }>`
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: #fff;
+  background-color: ${(props) => props.color};
+`;
+
+export const Value = styled.span<{ color: string }>`
+  color: ${(props) => props.color};
+`;
