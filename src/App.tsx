d2a851866d3b591200240d0486913efdb227881b
Khaled <khalegjr@gmail.com>
2023-02-05 03:20:48 -0300

    Recebe Listas de elementos e data atual

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
+import { items } from "../data/items";
 import { Body, Container, Header, HeaderText } from "./App.styles";
+import { getCurrentMonth } from "./helpers/dateFilters";
+import { Item } from "./types/Item";
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+
+  useEffect(() => {}, [list, currentMonth]);
+
   return (
     <Container>
       <Header>
